f0b2b670bc2a0b4b5bcb830737ac6469c7fcec62
arshiyamars <arshiyamars@gmail.com>
2024-07-28 21:59:35 +0330

    add a loop for command

--- sshshell.py
+++ sshshell.py
@@ -11,14 +11,23 @@ password = getpass("Enter your Password: ")
 
 # Create an SSH client
 clt = paramiko.SSHClient()
-clt.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Corrected line
+#clt.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Corrected line
+
+clt.load_system_host_keys()
+
+
+
 
 # Connect to the host
 clt.connect(host, username=username, password=password)
 
 # Receive a command from the client
-command = input("=> ")
-stdin, stdout, stderr = clt.exec_command(command)
-print(stdout.read().decode())
+command = input("==> ")
+
+while command !="end" :
+
+    stdin, stdout, stderr = clt.exec_command(command)
+    print(stdout.read().decode())
+    command = input("==> ")
 
 
